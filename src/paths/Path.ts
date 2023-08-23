8527e1566a4df3cec014d85ec407e8f51a806fc5
Stephen Oldham <stevo@vdotgood.com>
2023-08-23 16:46:56 -0700

    WIP

--- src/paths/Path.ts
+++ src/paths/Path.ts
@@ -216,6 +216,42 @@ export abstract class Path {
     this.svgElement.setAttribute('height', `${height}`);
   }
 
+  adjustedBoundingRect(el) {
+    var rect = el.getBoundingClientRect();
+    var style = getComputedStyle(el);
+    var tx = style.transform;
+  
+    if (tx) {
+      var sx, sy, dx, dy;
+      if (tx.startsWith('matrix3d(')) {
+        var ta = tx.slice(9,-1).split(/, /);
+        sx = +ta[0];
+        sy = +ta[5];
+        dx = +ta[12];
+        dy = +ta[13];
+      } else if (tx.startsWith('matrix(')) {
+        var ta = tx.slice(7,-1).split(/, /);
+        sx = +ta[0];
+        sy = +ta[3];
+        dx = +ta[4];
+        dy = +ta[5];
+      } else {
+        return rect;
+      }
+  
+      var to = style.transformOrigin;
+      var x = rect.x - dx - (1 - sx) * parseFloat(to);
+      var y = rect.y - dy - (1 - sy) * parseFloat(to.slice(to.indexOf(' ') + 1));
+      var w = sx ? rect.width / sx : el.offsetWidth;
+      var h = sy ? rect.height / sy : el.offsetHeight;
+      return {
+        x: x, y: y, width: w, height: h, top: y, right: x + w, bottom: y + h, left: x
+      };
+    } else {
+      return rect;
+    }
+  }
+
   /**
    * Sets the div attributes for the positioning and width/height
    */
@@ -240,10 +276,11 @@ export abstract class Path {
 
     // Take into account the appendTo element's offset for correct positioning
     if (this.options.appendTo && this.options.appendTo !== document.body) {
-      const offsetYAppended = this.options.appendTo.getBoundingClientRect().y;
-      const offsetXAppended = this.options.appendTo.getBoundingClientRect().x;
+      const rect = this.adjustedBoundingRect(this.options.appendTo);
+      const offsetYAppended = rect.y;
+      const offsetXAppended = rect.x;
 
-      // console.log(offsetYAppended, offsetXAppended)
+      console.log(offsetYAppended, offsetXAppended)
 
       this.containerDiv.style.top = `${top - offsetYAppended}px`;
       this.containerDiv.style.left = `${left - offsetXAppended}px`;
